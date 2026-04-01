ed3feaf65cb3008df8bcd7e0fd690de7a11f5e23
urkomen <urkomen@outlook.com>
2026-04-01 15:28:27 +0200

    Murder

--- 2-Data_Analysis/8-BBDD/SQL/Practica/4-Murder_game/murder_sql.sql
+++ 2-Data_Analysis/8-BBDD/SQL/Practica/4-Murder_game/murder_sql.sql
@@ -1,4 +1,26 @@
 -- Primer vistazo
 SELECT name 
   FROM sqlite_master
- where type = 'table'
+ where type = 'table';
+ 
+
+-- Vemos cada tabla
+select * from crime_scene_report;
+select date as Fecha, type as Tipo, city as Ciudad, description as Detalles 
+    from crime_scene_report
+    where type = 'murder'
+      and date = '20180115'
+      and city = 'SQL City';
+      
+
+
+
+
+select * from drivers_license;
+select * from facebook_event_checkin;
+select * from get_fit_now_check_in;
+select * from get_fit_now_member;
+select * from income;
+select * from interview;
+select * from get_fit_now_member;
+select * from person;
